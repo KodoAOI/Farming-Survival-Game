ccd07f4da29a7e48574c87ae9aa40b5acb39f0d8
Arigiri <63282838+Arigiri@users.noreply.github.com>
2022-07-26 14:27:37 +0700

    Commit

--- Farming Survival Game/Assets/Scripts/PlayerScripts/PlayerController.cs
+++ Farming Survival Game/Assets/Scripts/PlayerScripts/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update

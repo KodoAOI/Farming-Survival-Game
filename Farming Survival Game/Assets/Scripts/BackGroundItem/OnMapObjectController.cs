3ea5f7541aa24d1a36000fa99e60f67e118efd3a
Arigiri <63282838+Arigiri@users.noreply.github.com>
2022-07-31 16:32:11 +0700

    Fix

--- Farming Survival Game/Assets/Scripts/BackGroundItem/OnMapObjectController.cs
+++ Farming Survival Game/Assets/Scripts/BackGroundItem/OnMapObjectController.cs
@@ -24,12 +24,17 @@ public class OnMapObjectController : MonoBehaviour
     }
     private void DropItem()
     {
+        PlayerController m_Player = FindObjectOfType<PlayerController>();
+        Vector3 SpawnPoint = m_Player.RandomPointInAnnulus(m_Player.transform.position, 1f, 1.25f);
         foreach(CollectableObjectController item in ItemsDropper)
         {
-            Vector3 spawnLocation = transform.position;
-            Vector3 spawnOffset = UnityEngine.Random.insideUnitCircle * 1.25f;
-            CollectableObjectController droppedItem = item.GetPool().Spawn(spawnLocation + spawnOffset, FindObjectOfType<CollectableObjectsPool>().transform);//Instantiate(item, spawnLocation + spawnOffset, Quaternion.identity);
-            droppedItem.rb2d.AddForce(spawnOffset * .2f, ForceMode2D.Impulse);
+            Vector3 SpawnOffset = UnityEngine.Random.insideUnitCircle * 0.1f; 
+            m_Player.DropAllItem(item, SpawnPoint + SpawnOffset);
+            
+            // Vector3 spawnLocation = transform.position;
+            // Vector3 spawnOffset = UnityEngine.Random.insideUnitCircle * 1.25f;
+            // CollectableObjectController droppedItem = item.GetPool().Spawn(spawnLocation + spawnOffset, FindObjectOfType<CollectableObjectsPool>().transform);//Instantiate(item, spawnLocation + spawnOffset, Quaternion.identity);
+            // droppedItem.rb2d.AddForce(spawnOffset * .2f, ForceMode2D.Impulse);
         }
     }
 }
